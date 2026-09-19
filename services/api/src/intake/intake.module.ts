10c943a1683ef18e32bdb5301dd10a6b0b55c487
webbyadnan <rtwnoyan@gmail.com>
2026-09-19 14:55:54 +0500

    fix: load auth dependencies for intake routes

--- services/api/src/intake/intake.module.ts
+++ services/api/src/intake/intake.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
 import { IntakeController } from "./intake.controller";
 import { IntakeService } from "./intake.service";
 
-@Module({ controllers: [IntakeController], providers: [IntakeService] })
+@Module({ imports: [AuthModule], controllers: [IntakeController], providers: [IntakeService] })
 export class IntakeModule {}
